1e0b9f13544f5e7d711d5d097bce40e1349613b4
Adrian Gallardo <adrian.f.gallardo@gmail.com>
2020-10-28 07:18:28 -0600

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Adrian Gallardo on 28/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
