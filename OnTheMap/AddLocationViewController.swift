d895279db81055c331988c7c58c8c979cc6c61b8
Adrian Gallardo <adrian.f.gallardo@gmail.com>
2020-11-09 16:56:05 -0600

    Add code for validate location and mediaUrl

--- OnTheMap/AddLocationViewController.swift
+++ OnTheMap/AddLocationViewController.swift
@@ -6,10 +6,64 @@
 //
 
 import UIKit
+import MapKit
 
 class AddLocationViewController: UIViewController {
 	
+	@IBOutlet weak var locationTextField: UITextField!
+	@IBOutlet weak var mediaUrlTextField: UITextField!
+	@IBOutlet weak var findLocationButton: UIButton!
+
 	override func viewWillAppear(_ animated: Bool) {
 		tabBarController?.tabBar.isHidden = true
 	}
+
+	@IBAction func findLocation(_ sender: Any) {
+		guard let location = locationTextField.text, !location.isEmpty else {
+			messageAlert("Please insert a Location")
+			locationTextField.becomeFirstResponder()
+			return
+		}
+		guard let mediaUrl = mediaUrlTextField.text, !mediaUrl.isEmpty else {
+			messageAlert("Please insert an URL to share")
+			mediaUrlTextField.becomeFirstResponder()
+			return
+		}
+		findCoordinate(addressString: location, completionHandler: self.handleGetCoordinate(coordinates:error:))
+	}
+
+	func messageAlert(_ message: String) {
+		let alertVC = UIAlertController(title: "Attention", message: message, preferredStyle: .alert)
+		alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		self.present(alertVC, animated: true)
+	}
+
+	func findCoordinate(addressString: String, completionHandler: @escaping (CLLocationCoordinate2D, Error?) -> Void ) {
+		let geocoder = CLGeocoder()
+		geocoder.geocodeAddressString(addressString) { (placemarks, error) in
+			if error == nil {
+				if let placemark = placemarks?[0] {
+					let location = placemark.location!
+					completionHandler(location.coordinate, nil)
+					return
+				}
+			}
+			completionHandler(kCLLocationCoordinate2DInvalid, error)
+		}
+	}
+
+	func handleGetCoordinate(coordinates: CLLocationCoordinate2D, error: Error?) {
+		if let error = error {
+			let alertVC = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+			alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+			show(alertVC, sender: nil)
+			return
+		}
+		if let findLocationVC = self.storyboard!.instantiateViewController(withIdentifier: "findLocationViewController") as? FindLocationViewController {
+			findLocationVC.coordinates = coordinates
+			findLocationVC.mediaUrl = self.mediaUrlTextField.text ?? ""
+			self.navigationController!.pushViewController(findLocationVC, animated: true)
+		}
+	}
+
 }
--- OnTheMap/FindLocationViewController.swift
+++ OnTheMap/FindLocationViewController.swift
@@ -6,9 +6,62 @@
 //
 
 import UIKit
+import MapKit
 
-class FindLocationViewController: UIViewController {
+class FindLocationViewController: UIViewController, MKMapViewDelegate {
+	@IBOutlet weak var mapView: MKMapView!
+	var coordinates: CLLocationCoordinate2D?
+	var mediaUrl: String?
+
 	override func viewWillAppear(_ animated: Bool) {
 		tabBarController?.tabBar.isHidden = true
+
+		if let coordinates = coordinates {
+			let annotation = MKPointAnnotation()
+			annotation.coordinate = coordinates
+			annotation.title = "HOLA MUNDO"
+			annotation.subtitle = self.mediaUrl
+			mapView.addAnnotation(annotation)
+		}
+	}
+
+	@IBAction func finish(_ sender: Any) {
+
+	}
+
+	// MARK: - MKMapViewDelegate
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		let reuseId = "pin"
+		var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+		if pinView == nil {
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+			pinView!.canShowCallout = true
+			pinView!.pinTintColor = .red
+			pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+		} else {
+			pinView!.annotation = annotation
+		}
+
+		return pinView
+	}
+
+	// This delegate method is implemented to respond to taps. It opens the system browser
+	// to the URL specified in the annotationViews subtitle property.
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
+							 calloutAccessoryControlTapped control: UIControl) {
+		print("tap")
+		if control == view.rightCalloutAccessoryView {
+			guard let subtitle = view.annotation?.subtitle else {
+				return
+			}
+			guard let mediaString = subtitle else {
+				return
+			}
+			guard let mediaUrl = URL(string: mediaString) else {
+				return
+			}
+			UIApplication.shared.open(mediaUrl)
+		}
 	}
 }
